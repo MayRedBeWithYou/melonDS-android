diegolix29/melonDS-Trigger-Android

Allow achievement progress to be visualized in achievements list while emulator is running

app/src/main/java/me/magnum/melonds/ui/emulator/EmulatorRetroAchievementsViewModel.kt
@@ -2,20 +2,21 @@ package me.magnum.melonds.ui.emulator
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import me.magnum.melonds.domain.model.retroachievements.RAEvent
+import me.magnum.melonds.domain.model.retroachievements.RAUserAchievement
 import me.magnum.melonds.domain.model.rom.Rom
 import me.magnum.melonds.domain.repositories.RetroAchievementsRepository
 import me.magnum.melonds.domain.repositories.SettingsRepository
 import me.magnum.melonds.domain.services.EmulatorManager
-import me.magnum.melonds.extensions.removeFirst
 import me.magnum.melonds.impl.emulator.EmulatorSession
-import me.magnum.melonds.ui.common.viewmodel.RetroAchievementsViewModel
-import me.magnum.rcheevosapi.model.RAAchievement
+import me.magnum.melonds.ui.common.achievements.ui.model.AchievementUiModel
+import me.magnum.melonds.ui.common.achievements.viewmodel.RetroAchievementsViewModel
+import me.magnum.melonds.ui.romdetails.model.AchievementBucketUiModel
 import javax.inject.Inject
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Instant
 
 @HiltViewModel
 class EmulatorRetroAchievementsViewModel @Inject constructor(
@@ -25,29 +26,21 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
     private val emulatorManager: EmulatorManager,
 ) : RetroAchievementsViewModel(retroAchievementsRepository, settingsRepository) {
 
-    private val _activeChallenges = MutableStateFlow<List<RAAchievement>>(emptyList())
-    val activeChallenges = _activeChallenges.asStateFlow()
+    private class TimedUnlockedAchievement(
+        val achievementId: Long,
+        val unlockedAt: Instant,
+    )
+
+    private var activeChallengesIds = emptyList<Long>()
+    private var recentlyUnlockedAchievements = emptyList<TimedUnlockedAchievement>()
 
     init {
         viewModelScope.launch {
             emulatorManager.observeRetroAchievementEvents().collect { event ->
                 when (event) {
-                    is RAEvent.OnAchievementPrimed -> {
-                        retroAchievementsRepository.getAchievement(event.achievementId).onSuccess { primedAchievement ->
-                            if (primedAchievement != null) {
-                                _activeChallenges.update {
-                                    it + primedAchievement
-                                }
-                            }
-                        }
-                    }
-                    is RAEvent.OnAchievementUnPrimed -> {
-                        _activeChallenges.update {
-                            it.toMutableList().apply {
-                                removeFirst { it.id == event.achievementId }
-                            }
-                        }
-                    }
+                    is RAEvent.OnAchievementPrimed -> activeChallengesIds += event.achievementId
+                    is RAEvent.OnAchievementUnPrimed -> activeChallengesIds -= event.achievementId
+                    is RAEvent.OnAchievementTriggered -> recentlyUnlockedAchievements += TimedUnlockedAchievement(event.achievementId, Clock.System.now())
                     else -> { /* no-op */ }
                 }
             }
@@ -55,9 +48,7 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
     }
 
     fun onSessionReset() {
-        _activeChallenges.update {
-            emptyList()
-        }
+        activeChallengesIds = emptyList()
     }
 
     override fun getRom(): Rom {
@@ -67,5 +58,41 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
         }
 
         return romSession.rom
+    }
+
+    override suspend fun buildAchievementBuckets(achievements: List<RAUserAchievement>): List<AchievementBucketUiModel> {
+        val now = Clock.System.now()
+        return retroAchievementsRepository.getRuntimeUserAchievements(achievements).groupingBy { runtimeAchievement ->
+            when {
+                runtimeAchievement.userAchievement.isUnlocked -> {
+                    val recentlyUnlockedAchievement = recentlyUnlockedAchievements.firstOrNull { it.achievementId == runtimeAchievement.userAchievement.achievement.id }
+                    if (recentlyUnlockedAchievement != null && (now - recentlyUnlockedAchievement.unlockedAt) < 10.minutes) {
+                        AchievementBucketUiModel.Bucket.RecentlyUnlocked
+                    } else {
+                        AchievementBucketUiModel.Bucket.Unlocked
+                    }
+                }
+                activeChallengesIds.any { it == runtimeAchievement.userAchievement.achievement.id } -> AchievementBucketUiModel.Bucket.ActiveChallenges
+                runtimeAchievement.relativeProgress() >= 0.8f -> AchievementBucketUiModel.Bucket.AlmostThere
+                else -> AchievementBucketUiModel.Bucket.Locked
+            }
+        }.aggregate { _, accumulator: MutableList<AchievementUiModel>?, element, _ ->
+            val achievementUiModel = AchievementUiModel.RuntimeAchievementUiModel(element)
+            accumulator?.apply {
+                add(achievementUiModel)
+            } ?: mutableListOf(achievementUiModel)
+        }.map {
+            val achievements = if (it.key == AchievementBucketUiModel.Bucket.RecentlyUnlocked) {
+                // Sort recently unlocked achievements
+                it.value.sortedByDescending { achievement ->
+                    recentlyUnlockedAchievements.firstOrNull { it.achievementId == achievement.actualAchievement().id }?.unlockedAt
+                }
+            } else {
+                it.value
+            }
+            AchievementBucketUiModel(it.key, achievements)
+        }.sortedBy {
+            it.bucket.displayOrder
+        }
     }
 }

MayRedBeWithYou/melonDS-android

app/src/main/java/me/magnum/melonds/ui/emulator/EmulatorRetroAchievementsViewModel.kt
@@ -2,22 +2,23 @@ package me.magnum.melonds.ui.emulator
 
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import me.magnum.melonds.domain.model.retroachievements.RAEvent
+import me.magnum.melonds.domain.model.retroachievements.RAUserAchievement
 import me.magnum.melonds.domain.model.rom.Rom
 import me.magnum.melonds.domain.repositories.RetroAchievementsRepository
 import me.magnum.melonds.domain.repositories.SettingsRepository
 import me.magnum.melonds.domain.services.EmulatorManager
-import me.magnum.melonds.extensions.removeFirst
 import me.magnum.melonds.impl.emulator.EmulatorSession
 import me.magnum.melonds.impl.retroachievements.offline.OfflineLedgerIntegrity
 import me.magnum.melonds.impl.retroachievements.offline.OfflineLedgerRepository
-import me.magnum.melonds.ui.common.viewmodel.RetroAchievementsViewModel
-import me.magnum.rcheevosapi.model.RAAchievement
+import me.magnum.melonds.ui.common.achievements.ui.model.AchievementUiModel
+import me.magnum.melonds.ui.common.achievements.viewmodel.RetroAchievementsViewModel
+import me.magnum.melonds.ui.romdetails.model.AchievementBucketUiModel
 import javax.inject.Inject
+import kotlin.time.Clock
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Instant
 
 @HiltViewModel
 class EmulatorRetroAchievementsViewModel @Inject constructor(
@@ -28,29 +29,21 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
     private val emulatorManager: EmulatorManager,
 ) : RetroAchievementsViewModel(retroAchievementsRepository, settingsRepository) {
 
-    private val _activeChallenges = MutableStateFlow<List<RAAchievement>>(emptyList())
-    val activeChallenges = _activeChallenges.asStateFlow()
+    private class TimedUnlockedAchievement(
+        val achievementId: Long,
+        val unlockedAt: Instant,
+    )
+
+    private var activeChallengesIds = emptyList<Long>()
+    private var recentlyUnlockedAchievements = emptyList<TimedUnlockedAchievement>()
 
     init {
         viewModelScope.launch {
             emulatorManager.observeRetroAchievementEvents().collect { event ->
                 when (event) {
-                    is RAEvent.OnAchievementPrimed -> {
-                        retroAchievementsRepository.getAchievement(event.achievementId).onSuccess { primedAchievement ->
-                            if (primedAchievement != null) {
-                                _activeChallenges.update {
-                                    it + primedAchievement
-                                }
-                            }
-                        }
-                    }
-                    is RAEvent.OnAchievementUnPrimed -> {
-                        _activeChallenges.update {
-                            it.toMutableList().apply {
-                                removeFirst { it.id == event.achievementId }
-                            }
-                        }
-                    }
+                    is RAEvent.OnAchievementPrimed -> activeChallengesIds += event.achievementId
+                    is RAEvent.OnAchievementUnPrimed -> activeChallengesIds -= event.achievementId
+                    is RAEvent.OnAchievementTriggered -> recentlyUnlockedAchievements += TimedUnlockedAchievement(event.achievementId, Clock.System.now())
                     else -> { /* no-op */ }
                 }
             }
@@ -58,9 +51,7 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
     }
 
     fun onSessionReset() {
-        _activeChallenges.update {
-            emptyList()
-        }
+        activeChallengesIds = emptyList()
     }
 
     override fun getRom(): Rom {
@@ -70,6 +61,41 @@ class EmulatorRetroAchievementsViewModel @Inject constructor(
         }
 
         return romSession.rom
+    }
+
+    override suspend fun buildAchievementBuckets(achievements: List<RAUserAchievement>): List<AchievementBucketUiModel> {
+        val now = Clock.System.now()
+        return retroAchievementsRepository.getRuntimeUserAchievements(achievements).groupingBy { runtimeAchievement ->
+            when {
+                runtimeAchievement.userAchievement.isUnlocked -> {
+                    val recentlyUnlockedAchievement = recentlyUnlockedAchievements.firstOrNull { it.achievementId == runtimeAchievement.userAchievement.achievement.id }
+                    if (recentlyUnlockedAchievement != null && (now - recentlyUnlockedAchievement.unlockedAt) < 10.minutes) {
+                        AchievementBucketUiModel.Bucket.RecentlyUnlocked
+                    } else {
+                        AchievementBucketUiModel.Bucket.Unlocked
+                    }
+                }
+                activeChallengesIds.any { it == runtimeAchievement.userAchievement.achievement.id } -> AchievementBucketUiModel.Bucket.ActiveChallenges
+                runtimeAchievement.relativeProgress() >= 0.8f -> AchievementBucketUiModel.Bucket.AlmostThere
+                else -> AchievementBucketUiModel.Bucket.Locked
+            }
+        }.aggregate { _, accumulator: MutableList<AchievementUiModel>?, element, _ ->
+            val achievementUiModel = AchievementUiModel.RuntimeAchievementUiModel(element)
+            accumulator?.apply {
+                add(achievementUiModel)
+            } ?: mutableListOf(achievementUiModel)
+        }.map {
+            val achievementsByBucket = if (it.key == AchievementBucketUiModel.Bucket.RecentlyUnlocked) {
+                it.value.sortedByDescending { achievement ->
+                    recentlyUnlockedAchievements.firstOrNull { timedAchievement -> timedAchievement.achievementId == achievement.actualAchievement().id }?.unlockedAt
+                }
+            } else {
+                it.value
+            }
+            AchievementBucketUiModel(it.key, achievementsByBucket)
+        }.sortedBy {
+            it.bucket.displayOrder
+        }
     }
 
     override suspend fun getPendingLedgerAchievementIds(rom: Rom): Set<Long> {